634ad55c20ef660afe499e37a9dce498a9a646c1
Kai <kzhu1@cps.edu>
2026-02-03 16:11:56 -0600

    intake roller

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,7 +2,15 @@ package frc.robot;
 
 public class Constants {
     public static final class Swerve {
+
         public static final double MAX_SPEED = 4.7;
         public static final double CONTROLLER_DEADBAND = 0.1;
+
+    }
+    public static final class IntakeRoller {
+        public static final int MOTOR_ID = 9;
+        public static final double INTAKE_POWER = 1.0;
+        
+        
     }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,28 +8,41 @@ import java.io.File;
 
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.IntakeRoller;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import swervelib.SwerveInputStream;
 
 public class RobotContainer {
   private final SwerveSubsystem m_swerveSubsystem = new SwerveSubsystem((new File(Filesystem.getDeployDirectory(),
-                                                                                "swerve/robot")));
-  private final CommandXboxController m_driverController = new CommandXboxController(0); 
+      "swerve/robot")));
+  private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
+  private final CommandXboxController m_driverController = new CommandXboxController(0);
 
   SwerveInputStream driveAngularVelocity = SwerveInputStream.of(m_swerveSubsystem.getSwerveDrive(),
-                                                              () -> m_driverController.getLeftY() * -1,
-                                                              () -> m_driverController.getLeftX() * -1)
-                                                          .withControllerRotationAxis(m_driverController::getRightX)
-                                                          .deadband(Constants.Swerve.CONTROLLER_DEADBAND)
-                                                          .scaleTranslation(0.8)
-                                                          .allianceRelativeControl(true);
+      () -> m_driverController.getLeftY() * -1,
+      () -> m_driverController.getLeftX() * -1)
+      .withControllerRotationAxis(m_driverController::getRightX)
+      .deadband(Constants.Swerve.CONTROLLER_DEADBAND)
+      .scaleTranslation(0.8)
+      .allianceRelativeControl(true);
 
   public RobotContainer() {
     configureBindings();
   }
 
   private void configureBindings() {
+    m_driverController.rightBumper().toggleOnTrue(new RunCommand(() -> {
+      m_intakeSubsystem.setIndexerPower(
+          IntakeRoller.INTAKE_POWER);
+    }, m_intakeSubsystem));
+
+    m_intakeSubsystem.setDefaultCommand(new RunCommand(() -> {
+      m_intakeSubsystem.setIndexerPower(0.0);
+    }, m_intakeSubsystem));
+
     Command driveFieldOrientedAngularVelocityCommand = m_swerveSubsystem.driveFieldOriented(driveAngularVelocity);
     m_swerveSubsystem.setDefaultCommand(driveFieldOrientedAngularVelocityCommand);
   }
