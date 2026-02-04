65ced847e0515ffc16425ea124b9d222542f031a
Ronan <ronan.glavin@proton.me>
2026-02-04 12:07:31 -0600

    Vision Testing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,19 +4,33 @@
 
 package frc.robot;
 
+import static frc.robot.Constants.Vision.*;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import swervelib.SwerveDrive;
+
+import org.photonvision.PhotonCamera;
+
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
 
+  private PhotonCamera camera;
+
   public Robot() {
     m_robotContainer = new RobotContainer();
   }
 
+  @Override
+  public void robotInit() {
+    camera = new PhotonCamera(kCameraName);
+  }
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
@@ -54,8 +68,50 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
 
+      // Read in relevant data from the Camera
+
+      boolean targetVisible = false;
+
+      double targetYaw = 0.0;
+
+      var results = camera.getAllUnreadResults();
+
+      if (!results.isEmpty()) {
+
+          // Camera processed a new frame since last
+
+          // Get the last one in the list.
+
+          var result = results.get(results.size() - 1);
+
+          if (result.hasTargets()) {
+
+              // At least one AprilTag was seen by the camera
+
+              for (var target : result.getTargets()) {
+
+                  if (target.getFiducialId() == 0) {
+
+                      // Found Tag 7, record its information
+
+                      targetYaw = target.getYaw();
+
+                      targetVisible = true;
+
+                  }
+
+              }
+
+          }
+
+      }
+      
+      SmartDashboard.putBoolean("Vision Target Visible", targetVisible);
+      SmartDashboard.putNumber("Vision Target Yaw", targetYaw);
+
+  }
   @Override
   public void teleopExit() {}
 
