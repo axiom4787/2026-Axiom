7aca7f5d38ad6b9feab965c734ee10dede3aa1da
Ronan <ronan.glavin@proton.me>
2026-02-13 15:56:16 -0600

    "Be the change you wish to see in the repo".  -- Mahatma Gandhi

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,12 +5,15 @@
 package frc.robot;
 
 import java.io.File;
+import java.util.function.BooleanSupplier;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.Hubs;
 import frc.robot.Constants.Swerve;
 import frc.robot.subsystems.SwerveSubsystem;
@@ -21,22 +24,17 @@ public class RobotContainer {
       "swerve/robot")));
   private final CommandXboxController m_driverController = new CommandXboxController(0);
 
+  private boolean aiming = false;
+
   SwerveInputStream driveAngularVelocity = SwerveInputStream.of(m_swerveSubsystem.getSwerveDrive(),
       () -> m_driverController.getLeftY() * -1,
       () -> m_driverController.getLeftX() * -1)
       .withControllerRotationAxis(m_driverController::getRightX)
       .deadband(Swerve.CONTROLLER_DEADBAND)
       .scaleTranslation(0.8)
-      .allianceRelativeControl(true);
-  SwerveInputStream driveHubAligned = SwerveInputStream.of(m_swerveSubsystem.getSwerveDrive(),
-      () -> m_driverController.getLeftY() * -1,
-      () -> m_driverController.getLeftX() * -1)
-      .deadband(Swerve.CONTROLLER_DEADBAND)
-      .scaleTranslation(0.8)
       .allianceRelativeControl(true)
-      .aim((DriverStation.getAlliance().isPresent()) && (DriverStation.getAlliance().get() == Alliance.Blue) ? Hubs.BLUE_HUB : Hubs.RED_HUB);
-
-  // TODO: make only one input stream and use aimWhile instead of aim
+      .aim((DriverStation.getAlliance().isPresent()) && (DriverStation.getAlliance().get() == Alliance.Blue) ? Hubs.BLUE_HUB : Hubs.RED_HUB)
+      .aimWhile(this::isAiming);
 
   public RobotContainer() {
     configureBindings();
@@ -44,10 +42,16 @@ public class RobotContainer {
 
   private void configureBindings() {
     Command driveFieldOrientedAngularVelocityCommand = m_swerveSubsystem.driveFieldOriented(driveAngularVelocity);
-    Command driveFieldOrientedHubAligned = m_swerveSubsystem.driveFieldOriented(driveHubAligned);
     m_swerveSubsystem.setDefaultCommand(driveFieldOrientedAngularVelocityCommand);
-    m_driverController.b().toggleOnTrue(driveFieldOrientedHubAligned); // press b to toggle regular/hub aligned driving
+    m_driverController.b().onTrue(new InstantCommand(() -> {
+      aiming = !aiming;
+    })); // press b to toggle regular/hub aligned driving
   }
+
+  private boolean isAiming() {
+    return aiming;
+  }
+
 
   public Command getAutonomousCommand() {
     return null;
