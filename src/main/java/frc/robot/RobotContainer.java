d139a5e5125d6d3c459c52413ee640e2485683b1
coderCH2 <69022677+coderCH2@users.noreply.github.com>
2026-02-16 13:42:14 -0600

    Simplify Your Life And Focus On The Important Choices

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,14 +9,12 @@ import java.io.File;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.Flywheel;
-import frc.robot.subsystems.FlywheelSubsystem;
 import frc.robot.Constants.IntakeArm;
 import frc.robot.Constants.IntakeRoller;
-import frc.robot.subsystems.IntakeArmSubsystem;
-import frc.robot.subsystems.IntakeRollerSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import swervelib.SwerveInputStream;
 
@@ -24,47 +22,25 @@ public class RobotContainer {
   private final SwerveSubsystem m_swerveSubsystem = new SwerveSubsystem((new File(Filesystem.getDeployDirectory(),
       "swerve/robot")));
   private final CommandXboxController m_driverController = new CommandXboxController(0);
-  private final FlywheelSubsystem m_flywheelSubsystem = new FlywheelSubsystem();
-
-  private final IntakeRollerSubsystem m_intakeRollerSubsystem = new IntakeRollerSubsystem();
-  private final IntakeArmSubsystem m_intakeArmSubsystem = new IntakeArmSubsystem();
 
   SwerveInputStream driveAngularVelocity = SwerveInputStream.of(m_swerveSubsystem.getSwerveDrive(),
       () -> m_driverController.getLeftY() * -1,
       () -> m_driverController.getLeftX() * -1)
-      .withControllerRotationAxis(m_driverController::getRightX)
+      .withControllerRotationAxis(() -> m_driverController.getRightX() * -1)
       .deadband(Constants.Swerve.CONTROLLER_DEADBAND)
       .scaleTranslation(0.8)
-      .allianceRelativeControl(true);
+      .allianceRelativeControl(false);
 
   public RobotContainer() {
     configureBindings();
   }
 
   private void configureBindings() {
-    m_driverController.rightBumper().toggleOnTrue(new RunCommand(() -> {
-      m_intakeRollerSubsystem.setRollerPower(IntakeRoller.INTAKE_POWER);
-      m_intakeArmSubsystem.setArmPower(IntakeArm.DOWN_POWER);
-    }, m_intakeRollerSubsystem));
 
-    m_intakeRollerSubsystem.setDefaultCommand(new RunCommand(() -> {
-      m_intakeRollerSubsystem.setRollerPower(0.0);
-      m_intakeArmSubsystem.setArmPower(IntakeArm.UP_POWER);
-    }, m_intakeRollerSubsystem));
+    Command driveFieldOrientedAngularVelocityCommand = new RunCommand(() -> m_swerveSubsystem.drive(driveAngularVelocity.get()), m_swerveSubsystem);
+    m_swerveSubsystem.setDefaultCommand(driveFieldOrientedAngularVelocityCommand);  
 
-    Command driveFieldOrientedAngularVelocityCommand = m_swerveSubsystem.driveFieldOriented(driveAngularVelocity);
-    m_swerveSubsystem.setDefaultCommand(driveFieldOrientedAngularVelocityCommand);
-  
-   // shooter enabled/disabled logic
-		m_driverController.y().toggleOnTrue(new RunCommand(() -> {
-			m_flywheelSubsystem.setDesiredSpeed(
-				SmartDashboard.getNumber("Shooter/Setpoint", 0));
-		}, m_flywheelSubsystem));
-
-		m_flywheelSubsystem.setDefaultCommand(new RunCommand(() -> {
-			m_flywheelSubsystem.setDesiredSpeed(0.0);
-		}, m_flywheelSubsystem));
-
+    m_driverController.a().onTrue(new InstantCommand(m_swerveSubsystem::zeroGyro, m_swerveSubsystem));
   }
 
   public Command getAutonomousCommand() {
